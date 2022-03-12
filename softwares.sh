2ace874c7fe8c600cb2ea351892ffb3f9c039129
vasanth064 <vasanthvdev@gmail.com>
2022-03-13 04:43:52 +0530

    Finally chrome, edge and vscode installs through script 🙂

--- softwares.sh
+++ softwares.sh
@@ -1,52 +1,41 @@
-#add User to SUDOERS
+#Add User to SUDOERS
 cp -vr ./Configs/sudoers /etc/
 
-#add Debian Testing Repo
+#Add Debian Testing Repo
 cp -vr ./Configs/sources.list /etc/apt/
+
+#Installing pre-requtites for External Sources
 sudo apt update -y
+sudo apt install git curl wget -y
 
-#config GRUB
-cp -vr ./Configs/dracula /usr/share/grub/themes/
-cp -vr ./Configs/grub /etc/default/
-sudo update-grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-#Install latest Gnome 
-sudo apt -t testing install gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-session 
-
-#apt Packages
-sudo apt update -y
-sudo apt install git preload vlc gimp gparted flatpak scrcpy adb snapd curl wget -y
-sudo apt install dkms ttf-mscorefonts-installer rar unrar libavcodec-extra snapd -y
-sudo apt install gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi zsh -y
-
+#Adding apt External Sources 
 #albert
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
-sudo apt update -y
-sudo apt install albert -y
 
 #vs code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
-sudo apt update -y
-sudo apt install code -y
 
 #chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update -y
-sudo apt-get install google-chrome-stable -y
 
 #edge
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+
+#Installing apt Packages
 sudo apt update -y
-sudo apt install microsoft-edge-stable -y
+sudo apt install preload vlc gimp gparted flatpak scrcpy adb snapd albert code apt-transport-https -y
+sudo apt install dkms ttf-mscorefonts-installer rar unrar libavcodec-extra snapd microsoft-edge-stable -y
+sudo apt install gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi zsh google-chrome-stable -y
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+
+#Removing Duplicates form apt Sources
+rm -f packages.microsoft.gpg
 sudo rm microsoft.gpg
 sudo rm -rv /etc/apt/sources.list.d/microsoft-edge-dev.list
 
@@ -63,7 +52,6 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install flathub org.nomacs.ImageLounge -y
 
 #Install and configure Virtual Manager
-sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
 sudo virsh net-start default
 sudo virsh net-autostart default
 sudo adduser vasanth libvirt
@@ -71,6 +59,9 @@ sudo adduser vasanth libvirt-qemu
 
 #NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+#Install latest Gnome <40 
+sudo apt -t testing install gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-session 
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -80,4 +71,11 @@ sudo apt auto-remove -y
 git config --global user.email "vasanthvdev@gmail.com"
 git config --global user.name "vasanth064"
 
+#config GRUB
+cp -vr ./Configs/dracula /usr/share/grub/themes/
+cp -vr ./Configs/grub /etc/default/
+sudo update-grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+./cleanDebain.sh
 sudo reboot
--- softwaresMin.sh
+++ softwaresMin.sh
@@ -1,6 +1,9 @@
 #add User to SUDOERS
 cp -vr ./Configs/sudoers /etc/
 
+#Add Debian Testing Repo
+cp -vr ./Configs/sources.list /etc/apt/
+
 #apt Packages
 sudo apt update -y
 sudo apt install git preload curl wget -y
@@ -9,22 +12,25 @@ sudo apt install git preload curl wget -y
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-sudo apt update -y
-sudo apt install microsoft-edge-stable -y
-sudo rm microsoft.gpg
-sudo rm -rv /etc/apt/sources.list.d/microsoft-edge-dev.list
 
 #vs code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https -y
+
 sudo apt update -y
-sudo apt install code -y
+sudo apt install apt-transport-https code microsoft-edge-stable -y
+
+#Removing Duplicates form apt Sources
+rm -f packages.microsoft.gpg
+sudo rm microsoft.gpg
+sudo rm -rv /etc/apt/sources.list.d/microsoft-edge-dev.list
 
 #NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+#Install latest Gnome <40 
+sudo apt -t testing install gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs gnome-session 
 
 sudo apt update -y
 sudo apt auto-remove -y
@@ -33,4 +39,6 @@ sudo apt auto-remove -y
 git config --global user.email "vasanthvdev@gmail.com"
 git config --global user.name "vasanth064"
 
+./cleanDebian.sh
 sudo reboot
+
