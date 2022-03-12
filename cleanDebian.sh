dbffb72d74e6186d024df3b039f83921a3ea2db5
vasanth064 <vasanthvdev@gmail.com>
2022-03-13 04:38:14 +0530

    Added more packages to remove 🗑️

--- cleanDebian.sh
+++ cleanDebian.sh
@@ -1,2 +1,4 @@
-sudo apt remove gnome-contacts gnome-weather gnome-clocks gnome-2048 gnome-maps aisleriot gnome-calendar gnome-chess gnome-documents five-or-more four-in-a-row hitori gnome-klotski cheese lightsoff gnome-mahjongg gnome-mines gnome-music gnome-nibbles quadrapassel gnome-robots shotwell gnome-sound-recorder gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex gnome-games iagno alacarte -y
+sudo apt remove gnome-contacts gnome-weather gnome-clocks gnome-2048 gnome-maps aisleriot gnome-calendar gnome-chess gnome-documents five-or-more four-in-a-row hitori gnome-klotski cheese lightsoff gnome-mahjongg gnome-mines gnome-music gnome-nibbles quadrapassel gnome-robots shotwell gnome-sound-recorder gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex gnome-games iagno alacarte xterm goldendict kasumi hdate-applet mozc-utils-gui mlterm xiterm+term-y
+
 sudo apt autoremove -y
+./softwares.sh
